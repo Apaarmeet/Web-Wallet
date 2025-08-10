2e0a065c293108ecbdb2e15caddc5118d00dddc0
apaarmeet <apaarmeet5000@gmail.com>
2025-08-10 14:17:54 +0530

    changed the location

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import { generateMnemonic } from 'bip39';
-import SolanaWallet from '../SolanaWallet';
-import EthWallet from '../EthWallet';
+import SolanaWallet from './SolanaWallet';
+import EthWallet from './EthWallet';
 
 function App() {
   const [mnemonic, setMnemonic] = useState("");
